e3f1d60fad2ad5830e4dc41be99db172e7941074
kwagmire <kwagmire999@gmail.com>
2025-09-16 17:11:05 +0100

    altered vehicle and role tables to add and remove some constraints

---
+++ migrations/00005_alter_vehicle_table.sql
@@ -0,0 +1,23 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE vehicles
+DROP CONSTRAINT vehicles_owner_id_key;
+
+ALTER TABLE vehicles
+ADD CONSTRAINT check_vehicle_year CHECK (year >= 2005);
+
+ALTER TABLE users
+ADD CONSTRAINT check_user_role CHECK (role IN ('driver', 'vehicle_owner', 'super_admin'));
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE vehicles
+ADD CONSTRAINT vehicles_owner_id_key UNIQUE (owner_id);
+
+ALTER TABLE vehicles
+DROP CONSTRAINT check_vehicle_year;
+
+ALTER TABLE users
+DROP CONSTRAINT check_user_role;
+-- +goose StatementEnd
